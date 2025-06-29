d153a67b07e16e6e985902e36824bf0537bd7d7e
Martin Pichler <martin.pichler83@gmail.com>
2025-06-29 15:20:09 +0200

    UI Anpassung Semesterdarstellung

--- Classes/view_semester.py
+++ Classes/view_semester.py
@@ -18,28 +18,30 @@ class SemesterFrame(ctk.CTkFrame):
                 frame.grid(row=j, column=0, sticky="nsew", padx=10, pady=10)
                 semester_number = (j + 1) + (i * 4)
 
-                label = ctk.CTkLabel(frame, text=f"SEMESTER {(j+1)+(i*4)}", font=("Roboto", 12, "bold"), text_color=TITLE_COLOR, anchor="w")
-                label.grid(padx=20, pady=20, sticky="w")
+                label = ctk.CTkLabel(frame, text=f"SEMESTER {(j+1)+(i*4)}", font=("Roboto", 14, "bold"), text_color=TITLE_COLOR, anchor="w")
+                label.grid(padx=15, pady=20, sticky="w")
 
                 frame.grid_rowconfigure(1, weight=1)
                 frame.grid_columnconfigure(0, weight=1)
 
                 # create treeview
                 style = ttk.Style()
+                style.theme_use('clam')
                 style.configure("Treeview", background=CANVAS_COLOR, relief="flat", borderwidth=0)
+                style.configure("Treeview.Heading", background=CANVAS_COLOR, foreground=TITLE_COLOR, font=("Roboto", 10, "bold"), borderwidth=0)
                 style.layout("Treeview", [('Treeview.treearea', {'sticky': 'nswe'})])
 
                 tree = ttk.Treeview(frame, columns=("modul_ID", "module", "ects", "grade"), show="headings", height=4, style="Treeview")
-                tree.heading("modul_ID", text="ID")
-                tree.heading("module", text="Modul")
-                tree.heading("ects", text="Ects")
-                tree.heading("grade", text="Note")
+                tree.heading("modul_ID", text="ID", anchor="w")
+                tree.heading("module", text="Modul", anchor="w")
+                tree.heading("ects", text="Ects", anchor="center")
+                tree.heading("grade", text="Note", anchor="center")
 
                 tree.column("modul_ID", width=10, anchor="w")
                 tree.column("ects", width=10, anchor="center")
                 tree.column("grade", width=10, anchor="center")
 
-                # Semester-Objekt aus study_data finden
+                # fill treeview with data
                 semester_obj = next((s for s in study_data.semesters if s.semester_number == semester_number), None)
 
                 if semester_obj:
@@ -47,8 +49,11 @@ class SemesterFrame(ctk.CTkFrame):
                         grade_str = ""
                         if module.assessment and module.assessment.grade is not None:
                             grade_str = f"{module.assessment.grade:.1f}"
-        
-                        tree.insert("", "end", values=(module.module_ID, module.module_name, str(module.module_ects), grade_str))
+
+                        if module.module_ID == "":
+                            tree.insert("", "end", values=(module.module_ID, module.module_name, "", ""))
+                        else:
+                            tree.insert("", "end", values=(module.module_ID, module.module_name, str(module.module_ects), grade_str))
 
                 # treeview postion
                 tree.grid(row=1, column=0, sticky="nsew", padx=10, pady=(0, 10))
